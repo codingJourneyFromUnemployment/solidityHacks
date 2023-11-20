7179104435f36c318917411852c10133a297b763
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-11-20 22:09:04 +0800

    project init

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    }
+  },
+  solidity: "0.8.20",
+};
