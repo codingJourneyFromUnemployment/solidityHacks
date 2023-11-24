615c2218d4545c35ee171556da4a35453fbbbf53
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-11-24 16:48:30 +0800

    delegatecall

--- contracts/AccessingPrivateData.sol
+++ contracts/AccessingPrivateData.sol
@@ -53,5 +53,56 @@ contract Vault {
     password = _password;
   }
 
-  
+  function addUser(bytes32 _password) public {
+    User memory user = User({
+      id: users.length,
+      password: _password
+    });
+
+    users.push(user);
+    idToUser[user.id] = user;
+  }
+
+  function getArrayLocation(
+    uint slot,
+    uint index,
+    uint elementSize
+  ) public pure returns (uint) {
+    return uint(keccak256(abi.encodePacked(slot))) + (index * elementSize);
+  }
+
+  function getMapLocation(uint slot, uint key) public pure returns (uint) {
+    return uint(keccak256(abi.encodePacked(key, slot)));
+  }
 }
+
+/*
+slot 0 -count
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", 0, console.log)
+slot 1 - u16, isTrue, owner
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", 1, console.log)
+slot 2 - password
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", 2, console.log)
+
+slot 6 -array length
+getArrayLocation(6, 0, 2)
+web3.utils.numberToHex("111414077815863400510004064629973595961579173665589224203503662149373724986687")
+Note: We can also use web3 to get data location
+web3.utils.soloditySha3({type: "uint", calue: 6})
+1st user
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", "0xf652222313e28459528d920b65115c16c04f3efc82aaedc97be59f3f377c0d3f", console.log)
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", "0xf652222313e28459528d920b65115c16c04f3efc82aaedc97be59f3f377c0d40", console.log)
+Note: use web3.toAscii to convert bytes32 to alphabet
+2nd user
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", "0xf652222313e28459528d920b65115c16c04f3efc82aaedc97be59f3f377c0d41", console.log)
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", "0xf652222313e28459528d920b65115c16c04f3efc82aaedc97be59f3f377c0d42", console.log)
+
+slot 7 - empty
+getMapLocation(7, 1)
+web3.utils.numberToHex("81222191986226809103279119994707868322855741819905904417953092666699096963112")
+Note: We can also use web3 to get data location
+web3.utils.soliditySha3({ type: "uint", value: 1 }, {type: "uint", value: 7})
+user 1
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", "0xb39221ace053465ec3453ce2b36430bd138b997ecea25c1043da0c366812b828", console.log)
+web3.eth.getStorageAt("0x534E4Ce0ffF779513793cfd70308AF195827BD31", "0xb39221ace053465ec3453ce2b36430bd138b997ecea25c1043da0c366812b829", console.log)
+*/
